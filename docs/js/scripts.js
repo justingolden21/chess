44677ce99336901d2cfeb3c420e9e7c25b6071a8
RGB Studios <justingolden21@gmail.com>
2021-02-26 20:20:07 -0800

    added chess.js and chessboard.js
    currently just randomchess as a building block. source: // https://chessboardjs.com/examples/5002
    
    images from https://chessboardjs.com/download

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,1 +1,22 @@
-window.onload = ()=> console.log('page loaded');
+// https://chessboardjs.com/examples/5002
+window.onload = ()=> {
+
+	let board = null;
+	let game = new Chess();
+
+	function makeRandomMove() {
+		let possibleMoves = game.moves()
+
+		if (game.game_over()) return;
+
+		let randomIdx = Math.floor(Math.random() * possibleMoves.length);
+		game.move(possibleMoves[randomIdx]);
+		board.position(game.fen());
+
+		window.setTimeout(makeRandomMove, 500);
+	}
+
+	board = Chessboard('board', 'start');
+
+	window.setTimeout(makeRandomMove, 500);
+}
