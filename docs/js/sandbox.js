5c36ca4bb25a6fc8cfe61581efe65e257ba3cec4
RGB Studios <justingolden21@gmail.com>
2021-03-04 23:34:37 -0800

    add sandbox export/import fen

--- docs/js/sandbox.js
+++ docs/js/sandbox.js
@@ -1,12 +1,31 @@
 $(()=> {
+	function updateFen() {
+		$('#sandbox-fen').val(sandboxBoard.fen());
+	}
+
 	let sandboxBoard = Chessboard('sandbox-board', {
 		draggable: true,
 		dropOffBoard: 'trash',
 		sparePieces: true,
 		pieceTheme: 'img/pieces/{piece}.svg',
 		position: 'start',
+		onSnapEnd: updateFen,
 	});
+
+	updateFen();
 
 	$('#sandbox-start-btn').click(sandboxBoard.start);
 	$('#sandbox-clear-btn').click(sandboxBoard.clear);
+
+	$('#sandbox-load-btn').click(()=> {
+		let fenpgn = $('#sandbox-load-input').val();
+		sandboxBoard.position(fenpgn);
+	});
+	$('#sandbox-load-input').keydown(e=> { if(e.which==13) $('#sandbox-load-btn').click(); /* on enter*/ });
+
+	$('#copy-sandbox-fen').click(()=> {
+		$('#sandbox-fen').select();
+		document.execCommand('copy');
+	});
+
 });
