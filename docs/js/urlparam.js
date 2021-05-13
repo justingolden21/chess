e0b87b9cc6e2012c2cd994e6f860b730094178c5
RGB Studios <justingolden21@gmail.com>
2021-05-12 21:08:01 -0700

    support loading in (and encoding) FEN

--- docs/js/urlparam.js
+++ docs/js/urlparam.js
@@ -1,11 +1,13 @@
 $(()=> {
 	let url = new URL(window.location.href);
 	let pgn = decodeURI(url.searchParams.get('pgn'));
+	let fen = decodeURI(url.searchParams.get('fen'));
 	if(pgn && game.load_pgn(pgn)) $('#link-btn').addClass('active');
+	else if(fen) game.load(fen);
 
 	update(); // has to be called after loading url param
 });
 
-function updateUrlParam(pgn) {
-	history.replaceState({}, '', '?pgn=' + encodeURI(pgn));
+function updateUrlParam(str, isPGN=true) {
+	history.replaceState({}, '', `?${isPGN?'pgn':'fen'}=` + encodeURI(str));
 }
